0d6c1b9d02ea04d303a9d07a3fa103246f6f4979
Chayapon Aunhachoke <caunhach@z1t1c2.42bangkok.com>
2023-11-23 22:38:46 +0700

    last updated 23/11/2023

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -10,4 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
 
+ClapTrap::ClapTrap(): _name("Player"), _hp(10), _ep(10), _atk(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+}
+
+ClapTrap::ClapTrap(std::string name): _hp(10), _ep(10), _atk(0)
+{
+	std::cout << "Constructor of " << """ << name << """ << " called" << std::endl;
+	this->_name = name;
+}
+
+ClapTrap	&ClapTrap::operator=(const ClapTrap &src)
+{
+	std::cout << "Assignation operator called" << std::endl;
+	this->_name = src._name;
+	this->_hp = src._hp;
+	this->_ep = src._ep;
+	this->_atk = src._atk;
+	return (*this);
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "deconstructor called" << std::endl;
+}
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -6,8 +6,33 @@
 /*   By: caunhach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 22:01:36 by caunhach          #+#    #+#             */
-/*   Updated: 2023/11/23 22:01:38 by caunhach         ###   ########.fr       */
+/*   Updated: 2023/11/23 22:11:06 by caunhach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLAP_TRAP_HPP
+# define CLAP_TRAP_HPP
 
+#include<iostream>
+#include<string>
+
+class	ClapTrap
+{
+	private:
+		std::string	_name;
+		unsigned int _hp;
+		unsigned int _ep;
+		unsigned int _atk;
+
+	public:
+		ClapTrap();
+		ClapTrap(const ClapTrap &src);
+		ClapTrap(std::string name);
+		~ClapTrap();
+		ClapTrap &operator=(const ClapTrap &src);
+		void attack(const std::string &target);
+		void takeDamage(unsigned int point);
+		void beRepaired(unsigned int point);
+};
+
+#endif
