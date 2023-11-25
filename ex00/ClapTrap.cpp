19898f61fee6bcd9ca64042a34c2e6af988656ab
caunhach <caunhach@LAPTOP-5U4J17VN.localdomain>
2023-11-25 20:11:12 +0700

    last updated 25/11/2023

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -19,8 +19,12 @@ ClapTrap::ClapTrap(): _name("Player"), _hp(10), _ep(10), _atk(0)
 
 ClapTrap::ClapTrap(const ClapTrap &src)
 {
+	static int i;
 	std::cout << "Copy constructor called" << std::endl;
-	*this = src;
+	this->_name = src._name + ".copy" + std::to_string(++i);
+	this->_hp = src._hp;
+	this->_ep = src._ep;
+	this->_atk = src._atk;
 }
 
 ClapTrap::ClapTrap(std::string name): _hp(10), _ep(10), _atk(0)
@@ -46,10 +50,53 @@ ClapTrap::~ClapTrap()
 
 void	ClapTrap::attack(const std::string& target)
 {
-	if (this->_ep < 0)
-		std::cout << this->_name << "doesn't have enough energy points" << std::endl;
+	if (this->_ep == 0)
+		std::cout << this->_name << " tries to attack " << target << " but doesn't have enough energy point" << std::endl;
 	else if (this->_hp == 0)
-		std::cout << this->_name << "doesn't have hit point left" << std::endl;
+	{
+		std::cout << this->_name << " can not attack because he is dead" << std::endl;
+		return;
+	}
 	else
+	{
 		std::cout << this->_name << " attacks " << target << ", causing " << this->_atk << " points of damage!" << std::endl;
+		this->_ep--;
+	}
+	std::cout << this->_name << " now has " << this->_hp << " and " << this->_ep << " remain" << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+	if (this->_hp == 0)
+	{
+		std::cout << this->_name << " dose not recieve any damage because he is already dead" << std::endl;
+		return;
+	}
+	else if (amount > this->_hp)
+		this->_hp = 0;
+	else
+		this->_hp -= amount;
+	std::cout << this->_name << " now has " << this->_hp << " and " << this->_ep << " remain" << std::endl;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->_hp == 0)
+	{
+		std::cout << this->_name << " can not be repaired because he is already dead" << std::endl;
+		return;
+	}
+	std::cout << this->_name << " was repaired for amount of " << amount << " hit points " << std::endl;
+	std::cout << this->_name << "'s hp changed from " << this->_hp << " to ";
+	if (this->_hp + amount < 10)
+	{
+		this->_hp += amount;
+		std::cout << this->_hp << std::endl;
+	}
+	else
+	{
+		this->_hp = 10;
+		std::cout << this->_hp << std::endl;
+		std::cout << this->_name << " now has full hp" << std::endl;
+	}
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -10,4 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ClapTrap.hpp"
 
+int	main()
+{
+	ClapTrap a;
+	ClapTrap b(a);
+	a.attack("Peter");
+	a.takeDamage(3);
+	ClapTrap c(a);
+	a.takeDamage(2);
+	b.takeDamage(1);
+	c.takeDamage(1);
+	b.takeDamage(9);
+	c.takeDamage(10);
+	c.takeDamage(1);
+	a.beRepaired(2);
+	a.beRepaired(7);
+	return (0);
+}
